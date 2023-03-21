9918b73e2425cb562a514bef21c419dabcd5e0ab
giorgiGrigolia <grigolialive@gmail.com>
2023-03-21 23:25:27 +0400

    CSS partially finished

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 const year = document.querySelector(".year");
 const month = document.querySelector(".month");
 let result = document.querySelector(".result");
-
 const currentYear = new Date().getFullYear();
 const currentMonth = new Date().getMonth() + 1;
-
 const submit = document.querySelector(".button");
 
-// year.addEventListener("input", function () {
-//   result.textContent = `${year.value * 365} days`;
-// });
-
+// Calculating the amount of days using user's input //
 submit.addEventListener("click", function () {
   if (currentMonth < month.value) {
-    result.textContent = `You're ${Math.trunc(
+    result.textContent = `YOU ARE ${Math.trunc(
       (currentYear - year.value) * 365.25 - (month.value - currentMonth) * 30
-    )} years old.`;
+    )} DAYS OLD`;
   } else if (currentMonth > month.value) {
-    result.textContent = `You're ${Math.trunc(
+    result.textContent = `YOU ARE ${Math.trunc(
       (currentYear - year.value) * 365.25 + (currentMonth - month.value) * 30
-    )} days old.`;
+    )} DAYS OLD`;
   } else {
-    result.textContent = `You're ${Math.trunc(
+    result.textContent = `YOU ARE ${Math.trunc(
       (currentYear - year.value) * 365.25
-    )} days old.`;
+    )} DAYS OLD`;
+  }
+});
+
+// Changing font color when inputs are not empty //
+year.addEventListener("input", function () {
+  if (year.value !== "") {
+    year.style.color = "#000";
+  }
+});
+
+month.addEventListener("input", function () {
+  if (month.value !== "") {
+    month.style.color = "#000";
   }
 });
